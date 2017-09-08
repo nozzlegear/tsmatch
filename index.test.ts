918d32bad5a1f9d954e8f8fbea2ea744161a8b69
Joshua Harms <joshua@nozzlegear.com>
2017-09-08 16:23:51 -0500

    Initial commit

---
+++ index.test.ts
@@ -0,0 +1,308 @@
+import { match, withCase } from './index';
+
+test("Matches raw value 5", () => {
+    const input = 5;
+    let matchedFour = false;
+    let matchedFive = false;
+
+    const result = match(input, [
+        withCase(4, val => {
+            matchedFour = true;
+
+            return val
+        }),
+        withCase(5, val => {
+            matchedFive = true;
+
+            return val
+        })
+    ])
+
+    expect(result).toBe(input);
+    expect(matchedFive).toBe(true);
+    expect(matchedFour).toBe(false);
+})
+
+test("Matches raw string value", () => {
+    const input = "hello world";
+    let matchedFirst = false;
+    let matchedSecond = false;
+
+    const result = match(input, [
+        withCase("foo", val => {
+            matchedFirst = true;
+
+            return val
+        }),
+        withCase("hello world", val => {
+            matchedSecond = true;
+
+            return val
+        })
+    ])
+
+    expect(result).toBe(input);
+    expect(matchedFirst).toBe(false);
+    expect(matchedSecond).toBe(true);
+})
+
+test("Matches true value", () => {
+    const input = true
+
+    const result = match<string | boolean>(input, [
+        withCase("foo", val => val),
+        withCase(false, val => val),
+        withCase(true, val => val)
+    ])
+
+    expect(result).toBe(input)
+})
+
+test("Matches false value", () => {
+    const input = false
+
+    const result = match<string | boolean>(input, [
+        withCase("foo", val => val),
+        withCase(false, val => val),
+        withCase(true, val => val)
+    ])
+
+    expect(result).toBe(input)
+})
+
+test("Returns manipulated result", () => {
+    const input = 5
+
+    const doubled = match<number>(input, [
+        withCase("foo", val => val + val),
+        withCase(4, val => val + val),
+        withCase(5, val => val + val)
+    ])
+
+    expect(doubled).toBe(input + input);
+})
+
+test("Only matches one case", () => {
+    const input = 5
+    const expected = 20
+
+    const result = match<number>(input, [
+        withCase(5, val => expected),
+        withCase(5, val => val)
+    ])
+
+    expect(result).toBe(expected);
+})
+
+test("Matches generic when case", () => {
+    const input = 117
+
+    const result = match<number | string>(input, [
+        withCase(val => val > 200 && val < 225, val => "incorrect"),
+        withCase(val => val > 100 && val < 125, val => val),
+    ])
+
+    expect(result).toBe(input)
+})
+
+test("Matches fallthrough case", () => {
+    const input = 117
+
+    const result = match<number>(input, [
+        withCase(5, val => 5),
+        withCase(10, val => 10),
+        withCase(val => val)
+    ])
+
+    expect(result).toBe(input)
+})
+
+test("Matches string type case", () => {
+    const input = "hello world"
+    const expected = "string found"
+
+    const result = match<string>(input, [
+        withCase("number", val => "number found"),
+        withCase("string", val => expected),
+        withCase("object", val => "object found"),
+        withCase("array", val => "array found"),
+        withCase("boolean", val => "boolean found"),
+        withCase(val => "fell through")
+    ])
+
+    expect(result).toBe(expected)
+})
+
+test("Matches number type case", () => {
+    const input = 5
+    const expected = "number found"
+
+    const result = match<string>(input, [
+        withCase("number", val => expected),
+        withCase("string", val => "string found"),
+        withCase("object", val => "object found"),
+        withCase("array", val => "array found"),
+        withCase("boolean", val => "boolean found"),
+        withCase(val => "fell through")
+    ])
+
+    expect(result).toBe(expected)
+})
+
+test("Matches object type case", () => {
+    const input = { hello: "world" }
+    const expected = "object found"
+
+    const result = match<string>(input, [
+        withCase("number", val => "number found"),
+        withCase("string", val => "string found"),
+        withCase("object", val => expected),
+        withCase("array", val => "array found"),
+        withCase("boolean", val => "boolean found"),
+        withCase(val => "fell through")
+    ])
+
+    expect(result).toBe(expected)
+})
+
+test("Matches array type case", () => {
+    const input = [0, 1, 2]
+    const expected = "array found"
+
+    const result = match<string>(input, [
+        withCase("number", val => "number found"),
+        withCase("string", val => "string found"),
+        withCase("object", val => "object found"),
+        withCase("array", val => expected),
+        withCase("boolean", val => "boolean found"),
+        withCase(val => "fell through")
+    ])
+
+    expect(result).toBe(expected)
+})
+
+test("Matches boolean type case", () => {
+    const input = true
+    const expected = "boolean found"
+
+    const result = match<string>(input, [
+        withCase("number", val => "number found"),
+        withCase("string", val => "string found"),
+        withCase("object", val => "object found"),
+        withCase("array", val => "array found"),
+        withCase("boolean", val => expected),
+        withCase(val => "fell through")
+    ])
+
+    expect(result).toBe(expected)
+})
+
+test("Matches type case with a when check", () => {
+    const input = "hello world this is a string"
+    const expected = "hello string found"
+
+    const result = match<string>(input, [
+        withCase("string", val => val.indexOf("hello") === 0, val => expected),
+        withCase("string", val => val.indexOf("foo") === 0, val => val),
+        withCase(val => "fell through")
+    ])
+
+    expect(result).toBe(expected);
+})
+
+test("Throws error when no match is found", () => {
+    function execute() {
+        match("hello world", [
+            withCase(5, val => "Found 5"),
+            withCase("hello", val => "Found hello")
+        ])
+    }
+
+    expect(execute).toThrow()
+})
+
+test("Catch-all in first position stops execution of other cases", () => {
+    const input = "hello world";
+    let changed = false;
+
+    match(input, [
+        withCase(val => val),
+        withCase(input, val => changed = true)
+    ])
+
+    expect(changed).toBe(false);
+})
+
+test.skip("Should match object properties", () => {
+    const input = { hello: "world", foo: "bar" }
+    const expected = "found expected case"
+
+    const result = match(input, [
+        withCase({ hello: "world" }, val => "Matched partial hello:world object"),
+        withCase({ hello: "world", foo: "baz" }, val => "Matched hello:world foo:baz object"),
+        withCase({ hello: "world", foo: "bar" }, val => expected)
+    ])
+
+    expect(result).toBe(expected)
+})
+
+test.skip("Should match object properties with a when check", () => {
+    const input = { hello: "world", foo: "bar", baz: 5 };
+    const expected = "found expected case"
+
+    const result = match(input, [
+        withCase({ hello: "world" }, val => "first case"),
+        withCase({ hello: "world", foo: "bar" }, val => (val as any).baz === 4, val => "second case"),
+        withCase({ hello: "world", foo: "bar" }, val => (val as any).baz === 5, val => expected)
+    ])
+
+    expect(result).toBe(expected)
+})
+
+test.skip("Should only match given case properties and disregard extras", () => {
+    const input = { hello: "world", foo: "bar", baz: 5 };
+    const expected = "found expected case"
+
+    const result = match(input, [
+        withCase({ hello: "world", foo: "bar", baz: 10 }, val => "first case"),
+        withCase({ hello: "world", foo: "bar" }, val => expected)
+    ])
+
+    expect(result).toBe(expected)
+})
+
+test.skip("Should match array entries", () => {
+    const input = [0, 2, 4, 6]
+    const expected = "found expected case"
+
+    const result = match(input, [
+        withCase([1, 3, 5], val => "first case"),
+        withCase([0, 3, 5], val => "second case"),
+        withCase([0, 2, 4, 6], val => expected)
+    ])
+
+    expect(result).toBe(expected);
+})
+
+test.skip("Should match array entries with a when check", () => {
+    const input = [0, 2, 4, 6]
+    const expected = "found expected case"
+
+    const result = match(input, [
+        withCase([0, 2, 4, 6], val => val.every(x => x % 2 !== 0), val => "first case"),
+        withCase([0, 2, 4, 6], val => val.every(x => x % 2 === 0), val => expected)
+    ])
+
+    expect(result).toBe(expected)
+})
+
+test.skip("Should only match given array entries and disregard extras", () => {
+    const input = [0, 2, 4, 6]
+    const expected = "found expected case"
+
+    const result = match(input, [
+        withCase([0, 2, 4], val => expected)
+    ])
+
+    expect(result).toBe(expected);
+})
---
+++ index.ts
@@ -0,0 +1,151 @@
+export interface Match {
+    matches: boolean;
+    giveValue: (value: any) => any
+}
+
+export type TryMatch = (givenValue: any) => Match
+export type OnMatch<T> = (matchedValue: T) => any
+export type CheckForMatch<T> = (potentialMatchedValue: T) => boolean
+type TypeString = "string" | "object" | "array" | "number" | "boolean" | "function"
+type RawValue = string | object | any[] | number | boolean | undefined | null
+
+function isTypeString(value: TypeString | any): value is TypeString {
+    switch (value as TypeString) {
+        case "array":
+        case "boolean":
+        case "number":
+        case "object":
+        case "string":
+        case "function":
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+function matchTypeString(expectedType: TypeString, givenValue: any) {
+    // Because an array returns object, we must specifically test that givenValue is not an array when expectedType is object
+    const isArray = Array.isArray(givenValue);
+
+    switch (expectedType) {
+        case "array":
+            return isArray;
+
+        case "object":
+            return typeof (givenValue) === "object" && isArray === false
+
+        default:
+            return typeof (givenValue) === expectedType
+    }
+}
+
+function matchValue(expectedValue: RawValue, givenValue: any) {
+    // TODO: If the firstArg is an object we should check that all of its properties matches the given value.
+    // TODO: If the firstArg is an array, we should check that all of its entries match the given value's entries.
+    return expectedValue === givenValue;
+}
+
+export function match<T>(value: any, withThis: TryMatch[]): T {
+    const match = withThis.reduce<Match | undefined>((match, tryMatch) => {
+        if (match) {
+            return match
+        }
+
+        const output = tryMatch(value);
+
+        return output.matches ? output : undefined;
+    }, undefined)
+
+    if (!match) {
+        throw new Error(`No match found for value "${value}".`)
+    }
+
+    return match.giveValue(value);
+}
+
+export function withCase(onMatch: OnMatch<any>): TryMatch
+export function withCase(when: CheckForMatch<any>, onMatch: OnMatch<any>): TryMatch
+
+export function withCase<T>(onMatch: OnMatch<T>): TryMatch
+export function withCase<T>(when: CheckForMatch<any>, onMatch: OnMatch<T>): TryMatch
+
+export function withCase<T>(value: T, onMatch: OnMatch<T>): TryMatch
+export function withCase<T>(value: T, when: CheckForMatch<T>, onMatch: OnMatch<T>): TryMatch
+
+export function withCase(value: false, onMatch: OnMatch<false>): TryMatch
+export function withCase(value: false, when: CheckForMatch<false>, onMatch: OnMatch<false>): TryMatch
+
+export function withCase(value: true, onMatch: OnMatch<true>): TryMatch
+export function withCase(value: true, when: CheckForMatch<true>, onMatch: OnMatch<true>): TryMatch
+
+export function withCase(value: "string", onMatch: OnMatch<string>): TryMatch
+export function withCase(value: "string", when: CheckForMatch<string>, onMatch: OnMatch<string>): TryMatch
+
+export function withCase(value: "number", onMatch: OnMatch<number>): TryMatch
+export function withCase(value: "number", when: CheckForMatch<number>, onMatch: OnMatch<number>): TryMatch
+
+export function withCase(value: "object", onMatch: OnMatch<object>): TryMatch
+export function withCase(value: "object", when: CheckForMatch<object>, onMatch: OnMatch<object>): TryMatch
+
+export function withCase(value: "boolean", onMatch: OnMatch<boolean>): TryMatch
+export function withCase(value: "boolean", when: CheckForMatch<boolean>, onMatch: OnMatch<boolean>): TryMatch
+
+export function withCase(value: "array", onMatch: OnMatch<any[]>): TryMatch
+export function withCase(value: "array", when: CheckForMatch<any[]>, onMatch: OnMatch<any[]>): TryMatch
+export function withCase<T>(value: "array", onMatch: OnMatch<T[]>): TryMatch
+export function withCase<T>(value: "array", when: CheckForMatch<T[]>, onMatch: OnMatch<T[]>): TryMatch
+
+// function withCase<T>(when: (potentialMatchedValue: any) => potentialMatchedValue is T, onMatch: OnMatch<T>): TryMatch
+
+export function withCase(...args: any[]): TryMatch {
+    if (args.length === 0) {
+        throw new Error(`when function expects at least one argument.`)
+    }
+
+    const firstArg: OnMatch<any> | CheckForMatch<any> | TypeString | RawValue = args[0];
+
+    if (args.length === 1) {
+        if (typeof (firstArg) !== "function") {
+            throw new Error(`"when" function was given only one argument. Expected that argument to be an OnMatch callback function.`)
+        }
+
+        const onMatch: OnMatch<any> = firstArg
+
+        // Catch-all match has no condition and will always match
+        return (givenValue: any) => ({ matches: true, giveValue: onMatch })
+    }
+
+    const secondArg: OnMatch<any> | CheckForMatch<any> = args[1];
+
+    if (args.length === 2) {
+        // Second arg will be an OnMatch handler.
+        const onMatch: OnMatch<any> = secondArg;
+
+        if (typeof (firstArg) === "function") {
+            // First arg is a CheckForMatch handler.
+            const checkMatch: CheckForMatch<any> = firstArg
+
+            return (givenValue: any) => ({ matches: checkMatch(givenValue), giveValue: onMatch })
+        }
+
+        const value = firstArg;
+
+        if (isTypeString(value)) {
+            return (givenValue: any) => ({ matches: matchTypeString(value, givenValue), giveValue: onMatch })
+        }
+
+        // FirstArg is a value that should be checked for equality with the given value.
+        return (givenValue: any) => ({ matches: matchValue(value, givenValue), giveValue: onMatch });
+    }
+
+    // FirstArg is going to be a value or type string. SecondArg is going to be a CheckForMatch handler. ThirdArg is an OnMatch handler
+    const value: TypeString | RawValue = firstArg;
+    const checkMatch: CheckForMatch<any> = secondArg;
+    const onMatch: OnMatch<any> = args[2];
+
+    return (givenValue: any) => ({
+        matches: isTypeString(value) ? matchTypeString(value, givenValue) : matchValue(value, givenValue),
+        giveValue: onMatch
+    })
+}
